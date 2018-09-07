f996cd94d0b087f0613f8215ef5b9a043b3fee26
Christian Schwoerer <christian.schwoerer@novatec-gmbh.de>
2018-09-07 15:29:07 +0200

    Update Spring Boot/Cloud dependencies

--- zuul-edge-service/src/main/kotlin/info/novatec/gateway/zuuledgeservice/filter/AuthorizationFilter.kt
+++ zuul-edge-service/src/main/kotlin/info/novatec/gateway/zuuledgeservice/filter/AuthorizationFilter.kt
@@ -5,8 +5,8 @@ import com.netflix.zuul.context.RequestContext
 import com.netflix.zuul.exception.ZuulException
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants
 import org.springframework.cloud.netflix.zuul.util.ZuulRuntimeException
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpStatus
+import org.springframework.http.HttpHeaders.AUTHORIZATION
+import org.springframework.http.HttpStatus.BAD_REQUEST
 import org.springframework.stereotype.Component
 import org.springframework.web.util.WebUtils
 
@@ -14,16 +14,16 @@ import org.springframework.web.util.WebUtils
 class AuthorizationFilter : ZuulFilter() {
     override fun run(): Any? {
         val ctx = RequestContext.getCurrentContext()
-        val token = WebUtils.getCookie(ctx.request, "customer-Id")?.value ?: ""
+        val customerId = WebUtils.getCookie(ctx.request, "customer-Id")?.value ?: ""
 
-        if (token.isNotEmpty()) {
-            ctx.addZuulRequestHeader(HttpHeaders.AUTHORIZATION, token)
+        if (customerId.isNotEmpty()) {
+            ctx.addZuulRequestHeader(AUTHORIZATION, customerId)
             return null
         } else {
-            ctx.responseStatusCode = HttpStatus.BAD_REQUEST.value()
+            ctx.responseStatusCode = BAD_REQUEST.value()
             ctx.setSendZuulResponse(false)
             throw ZuulRuntimeException(
-                    ZuulException("Cookie 'customer-Id' missing.", HttpStatus.BAD_REQUEST.value(), null)
+                    ZuulException("Cookie 'customer-Id' missing.", BAD_REQUEST.value(), null)
             )
         }
     }
